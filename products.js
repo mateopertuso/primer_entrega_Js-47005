5d78f4bf929428452281d7309d9df1235908a568
Mateo Pertuso <mateopertuso.dev@gmail.com>
2023-09-04 15:08:55 -0300

    tercer entrega con DOM y localStorage

---
+++ products.js
@@ -0,0 +1,114 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Zapatos",
+        precio: 3200,
+        img:
+            "https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Chaqueta de jean",
+        precio: 1400,
+        img:
+            "https://images.pexels.com/photos/3649765/pexels-photo-3649765.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Jean",
+        precio: 2300,
+        img:
+            "https://media.istockphoto.com/photos/folded-blue-jeans-on-a-white-background-modern-casual-clothing-flat-picture-id1281304280",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Remera oversize",
+        precio: 790,
+        img:
+            "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "Zapatos nike",
+        precio: 4800,
+        img:
+            "https://images.pexels.com/photos/13595256/pexels-photo-13595256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 6,
+        nombre: "Mochila adidas",
+        precio: 2490,
+        img:
+            "https://images.pexels.com/photos/1102874/pexels-photo-1102874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 7,
+        nombre: "Campera algodon",
+        precio: 1200,
+        img:
+            "https://images.pexels.com/photos/4295985/pexels-photo-4295985.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        cantidad: 1,
+    },
+    {
+        id: 8,
+        nombre: "Gorra adidas",
+        precio: 1590,
+        img:
+            "https://images.pexels.com/photos/15759570/pexels-photo-15759570/free-photo-of-moda-mujer-oscuro-reloj-de-pulsera.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 9,
+        nombre: "Yeezy slide",
+        precio: 3690,
+        img:
+            "https://images.pexels.com/photos/8830049/pexels-photo-8830049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 10,
+        nombre: "Bolso tommy",
+        precio: 2990,
+        img:
+            "https://images.pexels.com/photos/12106423/pexels-photo-12106423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 11,
+        nombre: "Remera Hombre",
+        precio: 2290,
+        img:
+            "https://images.pexels.com/photos/17835505/pexels-photo-17835505/free-photo-of-moda-hombre-ropa-vestibulo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 12,
+        nombre: "Buzo hombre",
+        precio: 3190,
+        img:
+            "https://images.pexels.com/photos/17705005/pexels-photo-17705005/free-photo-of-hombre-gafas-de-sol-sombrero-en-pie.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 13,
+        nombre: "Top mujer",
+        precio: 1290,
+        img:
+            "https://images.pexels.com/photos/17748036/pexels-photo-17748036/free-photo-of-vacaciones-nina-en-pie-palmera.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+    {
+        id: 14,
+        nombre: "Campera mujer",
+        precio: 3490,
+        img:
+            "https://images.pexels.com/photos/16170/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        cantidad: 1,
+    },
+];
